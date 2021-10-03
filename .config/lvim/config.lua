dccffd784ac315ef2d7e092d9a279cd1821175cb
Jakub Reroń <kubareron@gmail.com>
2021-10-03 17:09:04 +0200

    feat(lvim): 🎸 ensure installed tresitter languages

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -87,8 +87,22 @@ lvim.builtin.nvimtree.hide_dotfiles = false
 lvim.builtin.nvimtree.ignore = { ".git", ".idea", ".vscode" }
 
 -- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = "maintained"
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
 lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
 lvim.builtin.treesitter.autotag.enable = true
 
 -- custom treesitter config
