e399522a96701a0156b5a94025dd66252db928d6
Jakub Reroń <kubareron@gmail.com>
2021-10-11 20:48:27 +0200

    feat(lvim): 🎸 declutter and improve LSP

--- .config/lvim/lua/config/lsp.lua
+++ .config/lvim/lua/config/lsp.lua
@@ -6,79 +6,68 @@ lvim.lsp.automatic_servers_installation=false
 -- you can set a custom on_attach function that will be used for all the language servers
 -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
 lvim.lsp.on_attach_callback = function(client, bufnr)
-	local function buf_set_option(...)
-		vim.api.nvim_buf_set_option(bufnr, ...)
-	end
-	--Enable completion triggered by <c-x><c-o>
-	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+  local function buf_set_option(...)
+    vim.api.nvim_buf_set_option(bufnr, ...)
+  end
+  --Enable completion triggered by <c-x><c-o>
+  buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+
+  if client.name == "tsserver" or client.name == "jsonls" then
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+  end
 end
+
+-- here's an example to disable formatting in "tsserver" and "jsonls"
 
 -- Langs
 lvim.lang.emmet.active = true
 lvim.lang.tailwindcss.active = true
 
--- lvim.lang.javascript = {
---         formatters = { { exe = "eslint_d" } },
---         linters = { { exe = "eslint_d" } },
--- }
+local formatters = require "lvim.lsp.null-ls.formatters"
+local linters = require "lvim.lsp.null-ls.linters"
 
--- lvim.lang.vue = {
---         formatters = { { exe = "eslint_d" } },
---         linters = { { exe = "eslint_d" } },
--- }
-
--- lvim.lang.typescript = {
---         formatters = { { exe = "eslint_d" } },
---         linters = { { exe = "eslint_d" } },
--- }
-
--- lvim.lang.html.formatters = { { exe = "prettierd" } }
-
--- lvim.lang.css = {
---         formatters = { { exe = "stylelint" } },
---         linters = { { exe = "stylelint" } },
--- }
-
--- lvim.lang.scss = {
---         formatters = { { exe = "stylelint" } },
---         linters = { { exe = "stylelint" } },
--- }
-
--- lvim.lang.json.formatters = { { exe = "prettierd" } }
-
-require('lvim.lsp.null-ls.formatters').setup({
-        {
-                exe = "eslint_d",
-                filetypes = {
-                        "javascript", "vue", "typescript", "javascriptreact", "typescriptreact", "json"
-                }
-        },
-        {
-                exe = "stylelint",
-                filetypes = {
-                       "css", "scss"
-                }
-        },
-        -- {
-        --         exe = "prettierd",
-        --         filetypes = {
-        --                 "html",
-        --         }
-        -- }
+formatters.setup({
+  {
+    exe = "eslint_d",
+    filetypes = {
+      "javascript",
+      "javascriptreact",
+      "vue",
+      "typescript",
+      "typescriptreact",
+    }
+  },
+  {
+    exe = "stylelint",
+    filetypes = {
+      "css", "scss", "less"
+    }
+  },
+  {
+    exe = "prettierd",
+    filetypes = {
+      "html", "markdown", "yaml", "json"
+    }
+  }
 })
 
 
-require('lvim.lsp.null-ls.linters').setup({
-        {
-                exe = "eslint_d",
-                filetypes = {
-                        "javascript", "vue", "typescript", "javascriptreact", "typescriptreact", "json"
-                }
-        },
-        {
-                exe = "stylelint",
-                filetypes = {
-                       "css", "scss"
-                }
-        },
+linters.setup({
+  {
+    exe = "eslint_d",
+    filetypes = {
+      "javascript",
+      "javascriptreact",
+      "vue",
+      "typescript",
+      "typescriptreact",
+    }
+  },
+  {
+    exe = "stylelint",
+    filetypes = {
+      "css", "scss", "less"
+    }
+  },
 })
