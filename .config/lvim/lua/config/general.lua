f3218f7f072cfcfbb7e38fc496aa8cc82208d272
Jakub Reroń <kubareron@gmail.com>
2021-10-19 19:28:06 +0200

    feat: 🎸 lvim config + theme

--- .config/lvim/lua/config/general.lua
+++ .config/lvim/lua/config/general.lua
@@ -4,8 +4,8 @@ lvim.format_on_save = true
 
 -- appearance
 -- lvim.colorscheme = "onedarker"
--- lvim.colorscheme = "tokyonight"
-lvim.colorscheme = "gruvbox"
+lvim.colorscheme = "tokyonight"
+-- lvim.colorscheme = "gruvbox"
 lvim.transparent_window = true
 
  -- debug
@@ -20,7 +20,7 @@ vim.opt.smarttab = true
 vim.opt.softtabstop = 2
 vim.opt.relativenumber = true
 vim.opt.hlsearch = false
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "100"
 vim.opt.incsearch = true
 vim.opt.lazyredraw = true
 vim.opt.magic = true
