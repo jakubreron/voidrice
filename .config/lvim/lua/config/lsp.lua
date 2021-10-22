7736dca7ac0b45deb3a70cf423f9e10acfc8eefc
Jakub Reroń <kubareron@gmail.com>
2021-10-22 21:01:14 +0200

    feat(aliases)

--- .config/lvim/lua/config/lsp.lua
+++ .config/lvim/lua/config/lsp.lua
@@ -18,40 +18,7 @@ lvim.lsp.on_attach_callback = function(client, bufnr)
     client.resolved_capabilities.document_formatting = false
     client.resolved_capabilities.document_range_formatting = false
   end
-
-
-local is_in_package_json = function(field)
-  if vim.fn.filereadable(vim.fn.getcwd() .. "/package.json") ~= 0 then
-    local package_json = vim.fn.json_decode(vim.fn.readfile "package.json")
-    if package_json[field] ~= nil then
-      return true
-    end
-    local dev_dependencies = package_json["devDependencies"]
-    if dev_dependencies ~= nil and dev_dependencies[field] ~= nil then
-      return true
-    end
-    local dependencies = package_json["dependencies"]
-    if dependencies ~= nil and dependencies[field] ~= nil then
-      return true
-    end
-  end
-  return false
 end
-
-local project_has_tailwindcss_dependency = function()
-  return (vim.fn.glob "tailwind*" ~= "" or is_in_package_json "tailwindcss")
-end
-
-if project_has_tailwindcss_dependency() == true then
-  require("lvim.lsp.manager").setup "tailwindcss"
-else
-  vim.list_extend(lvim.lsp.override, { "tailwindcss" })
-end
-end
-
--- Langs
--- lvim.lang.emmet.active = true
--- lvim.lang.tailwindcss.active = true
 
 local formatters = require "lvim.lsp.null-ls.formatters"
 local linters = require "lvim.lsp.null-ls.linters"
@@ -100,3 +67,42 @@ linters.setup({
     }
   },
 })
+
+--
+-- Does package.json file contain speficied configuration or dependency?
+-- (e.g. "prettier")
+-- IMPORTANT! package.json file is found only if current working directory (cwd)
+-- is in the root of the project, i.e. lvim was launched in the directory
+-- where package.json is or vim-rooter (or something similar) is activated
+--
+local is_in_package_json = function(field)
+  if vim.fn.filereadable(vim.fn.getcwd() .. "/package.json") ~= 0 then
+    local package_json = vim.fn.json_decode(vim.fn.readfile "package.json")
+    if package_json[field] ~= nil then
+      return true
+    end
+    local dev_dependencies = package_json["devDependencies"]
+    if dev_dependencies ~= nil and dev_dependencies[field] ~= nil then
+      return true
+    end
+    local dependencies = package_json["dependencies"]
+    if dependencies ~= nil and dependencies[field] ~= nil then
+      return true
+    end
+  end
+  return false
+end
+
+--
+-- Does current project contain tailwindcss configuration
+--
+local project_has_tailwindcss_dependency = function()
+  return (vim.fn.glob "tailwind*" ~= "" or is_in_package_json "tailwindcss")
+end
+
+--- Setup tailwindcss LSP if project has tailwindcss configuration
+if project_has_tailwindcss_dependency() == true then
+  require("lvim.lsp.manager").setup "tailwindcss"
+else
+  vim.list_extend(lvim.lsp.override, { "tailwindcss" })
+end
