e2f0cd13b80a5817e274258f4a2a81efcc0cee91
Jakub Reroń <kubareron@gmail.com>
2021-11-30 20:10:59 +0100

    feat(lvim): 🎸 editorconfig plugin

--- .config/lvim/lua/config/plugins.lua
+++ .config/lvim/lua/config/plugins.lua
@@ -8,6 +8,7 @@ lvim.plugins = {
   -- { "kshenoy/vim-signature" }, -- show the marks
   -- { "folke/tokyonight.nvim" }, -- nice theme
   -- { "rose-pine/neovim" }, -- nice theme
+  { "editorconfig/editorconfig-vim" }, -- .editorconfig support
   { "andweeb/presence.nvim" }, -- flex with VIM on the discord
   { "vimwiki/vimwiki" }, -- note taking plugin
   { "tpope/vim-repeat" }, -- better "."
