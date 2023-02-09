01d48e5e4ee9c531755cd4341a09c87f027fe6f3
Jakub Reroń <kubareron@gmail.com>
2023-02-09 13:32:54 +0100

    feat(darkman): switch gtk themes on the fly

--- .local/share/dark-mode.d/gtk-theme.sh
+++ .local/share/dark-mode.d/gtk-theme.sh
@@ -1,4 +1,6 @@
 gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 
-sed '2s/.*/gtk-theme-name=adw-gtk3-dark/' ~/.config/gtk-3.0/settings.ini > temp.ini && \mv temp.ini ~/.config/gtk-3.0/settings.ini
+sed --in-place --follow-symlinks 's/adw-gtk3/adw-gtk3-dark/' ~/.xsettingsd
+killall -HUP xsettingsd
+
--- .local/share/light-mode.d/gtk-theme.sh
+++ .local/share/light-mode.d/gtk-theme.sh
@@ -1,4 +1,6 @@
 gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3
 gsettings set org.gnome.desktop.interface color-scheme prefer-light
 
-sed '2s/.*/gtk-theme-name=adw-gtk3/' ~/.config/gtk-3.0/settings.ini > temp.ini && \mv temp.ini ~/.config/gtk-3.0/settings.ini
+sed --in-place --follow-symlinks 's/adw-gtk3-dark/adw-gtk3/' ~/.xsettingsd
+killall -HUP xsettingsd
+
