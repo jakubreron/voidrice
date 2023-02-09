f2e6af3c6355c84a655e6a6e4f5ff0694b2324cd
Jakub Reroń <kubareron@gmail.com>
2023-02-09 12:34:01 +0100

    fix: darkman config

--- .local/share/dark-mode.d/gtk-theme.sh
+++ .local/share/dark-mode.d/gtk-theme.sh
@@ -1,2 +1,2 @@
-gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && 
-gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark &&
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
--- .local/share/light-mode.d/gtk-theme.sh
+++ .local/share/light-mode.d/gtk-theme.sh
@@ -1,2 +1,2 @@
-gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3' &&
-gsettings set org.gnome.desktop.interface color-scheme prefer-light
+gsettings set org.gnome.desktop.interface color-scheme prefer-light &&
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
