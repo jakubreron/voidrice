59bff8c6938118d7f298fc6b46527886ebf7bf8d
Jakub Reroń <kubareron@gmail.com>
2023-02-09 13:38:35 +0100

    cleanup: remove adw-gtk3 gtk-theme from darkman

--- .local/share/dark-mode.d/gtk-theme.sh
+++ .local/share/dark-mode.d/gtk-theme.sh
@@ -1,4 +1,3 @@
-gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 
 sed --in-place --follow-symlinks 's/adw-gtk3/adw-gtk3-dark/' ~/.xsettingsd
--- .local/share/light-mode.d/gtk-theme.sh
+++ .local/share/light-mode.d/gtk-theme.sh
@@ -1,4 +1,3 @@
-gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3
 gsettings set org.gnome.desktop.interface color-scheme prefer-light
 
 sed --in-place --follow-symlinks 's/adw-gtk3-dark/adw-gtk3/' ~/.xsettingsd
