e2379e9d003e1f8daed21e0759460f226cca5c91
Jakub Reroń <kubareron@gmail.com>
2023-02-16 16:30:46 +0100

    refactor(mappings): all vim mappings to lua

--- .config/nvim/shared/mappings.lua
+++ .config/nvim/shared/mappings.lua
@@ -1,28 +1,25 @@
--- TODO: refactor to lua
-vim.cmd([[
-  " Jumplist
-  nnoremap <expr> k (v:count > 5 ? "m'" . v:count: "") . 'k'
-  nnoremap <expr> j (v:count > 5 ? "m'" . v:count: "") . 'j'
+local keymap = vim.keymap.set
+local silent = { silent = true }
+local expr = { expr = true, noremap = true, silent = true }
 
-  " Perform dot commands over visual blocks:
-  vnoremap . :normal .<CR>
+-- perform dot commands over visual blocks:
+keymap("v", ".", ":normal .<CR>", silent)
+-- save file as sudo on files that require root permission
+keymap("c", "w!!", "execute 'silent! write !sudo tee % >/dev/null' <bar> edit!")
+-- copy to end on Y
+keymap("n", "Y", "y$", silent)
 
-  " Save file as sudo on files that require root permission
-  " cnoremap w!! execute 'silent! write !sudo tee % >/dev/null' <bar> edit!
+-- keep it center
+keymap("n", "n", "nzzzv", silent)
+keymap("n", "N", "Nzzzv", silent)
+keymap("n", "J", "mzJ`z", silent)
+keymap("n", "<C-d>", "<C-d>zz", silent)
+keymap("n", "<C-u>", "<C-u>zz", silent)
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", silent)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", silent)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", silent)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", silent)
 
-  " Copy to end on Y
-  nnoremap Y y$
-
-  " Keep it center
-  nnoremap n nzzzv
-  nnoremap N Nzzzv
-  nnoremap J mzJ`z
-  nnoremap <C-d> <C-d>zz
-  nnoremap <C-u> <C-u>zz
-
-  " Shortcutting split navigation, saving a keypress:
-  map <C-h> <C-w>h
-  map <C-j> <C-w>j
-  map <C-k> <C-w>k
-  map <C-l> <C-w>l
-]])
+-- jumplist
+keymap("n", "k", 'v:count > 5 ? "m\'" .. v:count .. "k" : "k"', expr)
+keymap("n", "j", 'v:count > 5 ? "m\'" .. v:count .. "j" : "j"', expr)
