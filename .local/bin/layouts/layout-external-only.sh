0a8a60c32cdea1a3674b83ce42463f50e0485fc7
Jakub Reroń <kubareron@gmail.com>
2023-06-25 00:13:46 +0200

    refactor: extract disable-touchpad to separate script

--- .local/bin/layouts/layout-external-only.sh
+++ .local/bin/layouts/layout-external-only.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 xrandr --output DP1 --mode 1920x1080 --rate 144.00 --same-as LVDS1 --output LVDS1 --off
 
-if xinput --list | grep -q "TouchPad"; then
-  touchpad_id=$(xinput --list | grep "TouchPad" | sed -n "s/.*id=\([0-9]*\).*/\1/p")
-  xinput --disable "$touchpad_id"
+if command -v disable-touchpad >/dev/null 2>&1; then
+  disable-touchpad
 fi
