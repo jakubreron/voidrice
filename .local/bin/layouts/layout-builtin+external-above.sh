4a6a6a7d860bac7e3167907d73dece9c02d1c2c9
Jakub Reroń <kubareron@gmail.com>
2023-06-26 20:10:24 +0200

    config(rofi-layout): 144, 120 -> 60hz

--- .local/bin/layouts/layout-builtin+external-above.sh
+++ .local/bin/layouts/layout-builtin+external-above.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-xrandr --output DP1 --mode 1920x1080 --rate 144.00 --above LVDS1 --primary --output LVDS1 --mode 1920x1080 --rate 60.00
+xrandr --output DP1 --mode 1920x1080 --rate 60.00 --above LVDS1 --primary --output LVDS1 --mode 1920x1080 --rate 60.00
 
 if command -v touchpad >/dev/null 2>&1; then
   touchpad --disable
