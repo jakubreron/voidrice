ebd08b160f35069f431faa5ca11c2bba500d054c
Jakub Reroń <kubareron@gmail.com>
2023-08-02 21:33:47 +0200

    fix(layouts): display codes after migrating from xf86-video-intel to modesetting

--- .local/bin/layouts/layout-builtin-only.sh
+++ .local/bin/layouts/layout-builtin-only.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-xrandr --output LVDS1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VGA1 --off --output VIRTUAL1 --off
+xrandr --output LVDS-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-1 --off --output HDMI-2 --off --output HDMI-3 --off --output VGA-1 --off --output VIRTUAL-1 --off
 
 if command -v touchpad >/dev/null 2>&1; then
   touchpad --enable
--- .local/bin/layouts/layout-external-only.sh
+++ .local/bin/layouts/layout-external-only.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-xrandr --output DP1 --mode 1920x1080 --rate 60.00 --same-as LVDS1 --output LVDS1 --off
+xrandr --output DP-1 --mode 1920x1080 --rate 60.00 --same-as LVDS-1 --output LVDS-1 --off
 
 if command -v touchpad >/dev/null 2>&1; then
   touchpad --disable
