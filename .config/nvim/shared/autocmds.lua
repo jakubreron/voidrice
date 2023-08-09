3ec304f24ba1a22fed2b42f50aef812a42e66521
Jakub Reroń <kubareron@gmail.com>
2023-08-09 23:32:37 +0200

    config(nvim/autocmds): auto-commit only when buffer contains changes

--- .config/nvim/shared/autocmds.lua
+++ .config/nvim/shared/autocmds.lua
@@ -21,15 +21,15 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-	pattern = { ".env*", "**/node_modules/**" } ,
-  callback = function(args)
-    vim.diagnostic.disable(args.buf)
-  end
+	pattern = { ".env*", "**/node_modules/**" },
+	callback = function(args)
+		vim.diagnostic.disable(args.buf)
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-	pattern = { ".env*" } ,
-  command = "setlocal filetype=sh"
+	pattern = { ".env*" },
+	command = "setlocal filetype=sh",
 })
 
 local x_filetypes = { "xresources", "xdefaults" }
@@ -43,7 +43,11 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 })
 
 local auto_commit = function(type, scope)
-	return "!git add .; git commit -m '" .. type .. "(" .. scope .. "): ⚙️ auto-commit changes'; git pull && git push;"
+	return "!git add .; git commit -m '"
+		.. type
+		.. "("
+		.. scope
+		.. "): ⚙️ auto-commit changes'; git pull && git push;"
 end
 
 vim.api.nvim_create_autocmd({ "VimLeave" }, {
@@ -51,20 +55,34 @@ vim.api.nvim_create_autocmd({ "VimLeave" }, {
 	command = auto_commit("config", "bm-files") .. "shortcuts",
 })
 
+local function buffer_has_git_changes()
+	local filepath = vim.fn.expand("%:p")
+	local git_status = vim.fn.system("git diff --name-status " .. filepath)
+
+	return git_status ~= ""
+end
+
 vim.api.nvim_create_autocmd({ "VimLeave" }, {
 	pattern = vim.fn.expand("$VIMWIKI_DIR") .. "/*",
-	command = auto_commit("docs", "vimwiki"),
+	callback = function()
+		if buffer_has_git_changes() then
+			vim.cmd(auto_commit("docs", "vimwiki"))
+		end
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "VimLeave" }, {
-	pattern = vim.fn.expand("~") .. "/.config/shell/aliasrc*",
-	command = auto_commit("config", "alias"),
+	pattern = {
+		vim.fn.expand("~") .. "/.config/shell/aliasrc*",
+		vim.fn.expand("~") .. "/.config/shell/profile*",
+	},
+  callback = function ()
+		local filename = vim.fn.expand("%:t")
+		if buffer_has_git_changes() then
+			vim.cmd(auto_commit("config", filename))
+		end
+  end
 })
-vim.api.nvim_create_autocmd({ "VimLeave" }, {
-	pattern = vim.fn.expand("~") .. "/.config/shell/profile*",
-	command = auto_commit("config", "profile"),
-})
-
 vim.api.nvim_create_autocmd({ "VimLeave" }, {
 	pattern = "*/src/*/config.h",
 	command = "!sudo make install",
