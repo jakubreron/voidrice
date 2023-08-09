e009c6f41b77cd8f544ad5df86dd9ba13e30e373
Jakub Reroń <kubareron@gmail.com>
2023-08-09 22:10:48 +0200

    config(profile): ⚙️ auto-commit changes

--- .local/bin/layouts/layout-builtin+external-above.sh
+++ .local/bin/layouts/layout-builtin+external-above.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-xrandr --output DP-1 --mode 1920x1080 --rate 60.00 --above LVDS-1 --primary --output LVDS-1 --mode 1920x1080 --rate 60.00
+xrandr --output DP-1 --mode 1920x1080 --rate 144.00 --above LVDS-1 --primary --output LVDS-1 --mode 1920x1080 --rate 60.00
 notify-send "🖥️ External Display + 💻 Laptop Display"
 
 if command -v touchpad >/dev/null 2>&1; then
--- .local/bin/layouts/layout-external-only.sh
+++ .local/bin/layouts/layout-external-only.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-xrandr --output DP-1 --mode 1920x1080 --rate 60.00 --same-as LVDS-1 --output LVDS-1 --off
+xrandr --output DP-1 --mode 1920x1080 --rate 144.00 --same-as LVDS-1 --output LVDS-1 --off
 notify-send "🖥️ External Display"
 
 if command -v touchpad >/dev/null 2>&1; then
