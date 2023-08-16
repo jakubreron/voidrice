bb2665d97471eba37718da15dd7cd3113ea16bbe
Jakub Reroń <kubareron@gmail.com>
2023-08-16 15:36:56 +0200

    refactor(nvim/Eatchar): better names

--- .config/nvim/shared/iabbrev.lua
+++ .config/nvim/shared/iabbrev.lua
@@ -2,10 +2,10 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	pattern = { "*.js", "*.ts", "*.vue", "*.jsx", "*.tsx" },
 	callback = function()
     vim.cmd([[
-     func Eatchar(pat)
-       let c = nr2char(getchar(0))
-       return (c =~ a:pat) ? '' : c
-     endfunc
+     function! Eatchar(pat)
+       let char = nr2char(getchar(0))
+       return (char =~ a:pat) ? '' : char
+     endfunction
 
      iabbrev cl console.log()<Left><c-r>=Eatchar('\m\s\<bar>/')<CR>
      iabbrev cd console.debug()<Left><c-r>=Eatchar('\m\s\<bar>/')<CR>
