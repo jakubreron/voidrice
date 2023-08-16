fd0a5d152b84af8a5e813da7395e279f688a03c2
Jakub Reroń <kubareron@gmail.com>
2023-08-16 15:34:32 +0200

    fix(iabbrev): trailing space

--- .config/nvim/shared/iabbrev.lua
+++ .config/nvim/shared/iabbrev.lua
@@ -2,8 +2,13 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	pattern = { "*.js", "*.ts", "*.vue", "*.jsx", "*.tsx" },
 	callback = function()
     vim.cmd([[
-      iabbrev cl console.log( )<Left><Left>
-      iabbrev cd console.debug( )<Left><Left>
-    ]])
-	end,
+     func Eatchar(pat)
+       let c = nr2char(getchar(0))
+       return (c =~ a:pat) ? '' : c
+     endfunc
+
+     iabbrev cl console.log()<Left><c-r>=Eatchar('\m\s\<bar>/')<CR>
+     iabbrev cd console.debug()<Left><c-r>=Eatchar('\m\s\<bar>/')<CR>
+   ]])
+  end,
 })
