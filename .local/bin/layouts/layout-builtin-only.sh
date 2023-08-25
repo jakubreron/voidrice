ced25213dbff04e6cbdfd53806c8d74cbdbd3699
Jakub Reroń <kubareron@gmail.com>
2023-08-25 22:12:16 +0200

    config(profile.linux): ⚙️ auto-commit changes

--- .local/bin/layouts/layout-builtin-only.sh
+++ .local/bin/layouts/layout-builtin-only.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
-xrandr --output LVDS-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-1 --off --output HDMI-2 --off --output HDMI-3 --off --output VGA-1 --off --output VIRTUAL-1 --off
+if [[  "$STARTUP_DESKTOP" = "Wayland" ]]; then
+  wlr-randr --output LVDS-1 --on --mode 1920x1080 --pos 0,0 --transform normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-1 --off --output HDMI-2 --off --output HDMI-3 --off --output VGA-1 --off --output VIRTUAL-1 --off
+fi
+
+if [[ "$STARTUP_DESKTOP" = "X11" ]]; then
+  xrandr --output LVDS-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-1 --off --output HDMI-2 --off --output HDMI-3 --off --output VGA-1 --off --output VIRTUAL-1 --off
+
+  if command -v touchpad >/dev/null 2>&1; then
+    touchpad --enable
+  fi
+
+  if command -v setbg >/dev/null 2>&1; then
+    setbg
+  fi
+fi
+
 notify-send "💻 Laptop Display"
-
-if command -v touchpad >/dev/null 2>&1; then
-  touchpad --enable
-fi
-
-if command -v setbg >/dev/null 2>&1; then
-  setbg
-fi
--- .local/bin/layouts/layout-external-only.sh
+++ .local/bin/layouts/layout-external-only.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
-xrandr --output DP-2 --mode 1920x1080 --rate 60.00 --same-as LVDS-1 --output LVDS-1 --off
+if [[  "$STARTUP_DESKTOP" = "Wayland" ]]; then
+  wlr-randr --output DP-2 --on --mode 1920x1080 --pos 0,0 --transform normal --output LVDS-1 --off
+fi
+
+if [[ "$STARTUP_DESKTOP" = "X11" ]]; then
+  xrandr --output DP-2 --mode 1920x1080 --rate 60.00 --same-as LVDS-1 --output LVDS-1 --off
+
+  if command -v touchpad >/dev/null 2>&1; then
+    touchpad --disable
+  fi
+
+  if command -v setbg >/dev/null 2>&1; then
+    setbg
+  fi
+fi
+
 notify-send "🖥️ External Display"
-
-if command -v touchpad >/dev/null 2>&1; then
-  touchpad --disable
-fi
-
-if command -v setbg >/dev/null 2>&1; then
-  setbg
-fi
