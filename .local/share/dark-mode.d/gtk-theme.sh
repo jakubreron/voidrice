14a7b1a59f7f7ed722c8e46dc0233a514a9f952f
Jakub Reroń <kubareron@gmail.com>
2023-08-26 14:02:14 +0200

    feat: darkman support for tmux & alacritty

--- .local/share/dark-mode.d/gtk-theme.sh
+++ .local/share/dark-mode.d/gtk-theme.sh
@@ -2,7 +2,7 @@ gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 gsettings set org.gnome.desktop.interface gtk-theme "$GTK_DARK_THEME" # anki and other apps that depend on string "-dark" fix
 
 # old apps fix
-sed --in-place --follow-symlinks "s/'$GTK_LIGHT_THEME'/'$GTK_DARK_THEME'/" ~/.xsettingsd 
+sed --in-place --follow-symlinks "s/$GTK_LIGHT_THEME/$GTK_DARK_THEME/" ~/.xsettingsd 
 
 if [[ "$STARTUP_DESKTOP" = "X11" ]]; then
   killall -hup xsettingsd
--- .local/share/light-mode.d/gtk-theme.sh
+++ .local/share/light-mode.d/gtk-theme.sh
@@ -3,7 +3,7 @@ gsettings set org.gnome.desktop.interface gtk-theme "$GTK_LIGHT_THEME" # anki an
 
 # old apps fix
 #
-sed --in-place --follow-symlinks "s/'$GTK_DARK_THEME'/'$GTK_LIGHT_THEME'/" ~/.xsettingsd 
+sed --in-place --follow-symlinks "s/$GTK_DARK_THEME/$GTK_LIGHT_THEME/" ~/.xsettingsd 
 
 if [[ "$STARTUP_DESKTOP" = "X11" ]]; then
   killall -hup xsettingsd
