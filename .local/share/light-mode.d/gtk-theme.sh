bc59f98b4e05f0f843fa068dfb7fe60aea5f2cf2
Jakub Reroń <kubareron@gmail.com>
2023-08-26 15:20:00 +0200

    config: extends gtk settings with themes in darkman

--- .local/share/light-mode.d/gtk-theme.sh
+++ .local/share/light-mode.d/gtk-theme.sh
@@ -4,6 +4,11 @@ gsettings set org.gnome.desktop.interface gtk-theme "$GTK_LIGHT_THEME" # anki an
 # old apps fix
 sed --in-place --follow-symlinks "s/'$GTK_DARK_THEME'/'$GTK_LIGHT_THEME'/" ~/.xsettingsd 
 
+# multiple gtk versions
+ln -fs /usr/share/themes/Catppuccin-Latte-Standard-Blue-light/gtk-4.0/* ~/.config/gtk-4.0/
+ln -fs /usr/share/themes/Catppuccin-Latte-Standard-Blue-light/gtk-3.0/* ~/.config/gtk-3.0/
+ln -fs /usr/share/themes/Catppuccin-Latte-Standard-Blue-light/gtk-2.0/* ~/.config/gtk-2.0/
+
 if [[ "$STARTUP_DESKTOP" = "X11" ]]; then
   killall -hup xsettingsd
 fi
