0c2ba42b4efe4707f787b2c3435b802fb61347dd
Jakub Reroń <kubareron@gmail.com>
2023-08-26 23:34:27 +0200

    fix: darkman switcher for gtk themes v2

--- .local/share/light-mode.d/gtk-theme.sh
+++ .local/share/light-mode.d/gtk-theme.sh
@@ -2,14 +2,12 @@ gsettings set org.gnome.desktop.interface color-scheme prefer-light
 gsettings set org.gnome.desktop.interface gtk-theme "$GTK_LIGHT_THEME" # anki and other apps that depend on string "-dark" fix
 
 # old apps fix
-sed --in-place --follow-symlinks "s/'$GTK_DARK_THEME'/'$GTK_LIGHT_THEME'/" ~/.xsettingsd 
-sed --in-place --follow-symlinks "s/'$GTK_DARK_THEME'/'$GTK_LIGHT_THEME'/" ~/.config/gtk-4.0/settings.ini
+sed --in-place --follow-symlinks "s/$GTK_DARK_THEME/$GTK_LIGHT_THEME/" ~/.xsettingsd 
+sed --in-place --follow-symlinks "s/$GTK_DARK_THEME/$GTK_LIGHT_THEME/" ~/.config/gtk-4.0/settings.ini
 
 # multiple gtk versions
 ln -fs /usr/share/themes/$GTK_LIGHT_THEME/gtk-4.0/* ~/.config/gtk-4.0/
 ln -fs /usr/share/themes/$GTK_LIGHT_THEME/gtk-3.0/* ~/.config/gtk-3.0/
 ln -fs /usr/share/themes/$GTK_LIGHT_THEME/gtk-2.0/* ~/.config/gtk-2.0/
 
-if [[ "$STARTUP_DESKTOP" = "X11" ]]; then
-  killall -hup xsettingsd
-fi
+killall -hup xsettingsd
