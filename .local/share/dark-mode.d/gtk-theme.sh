9e980508551586779aedac5d849d7314a6960599
Jakub Reroń <kubareron@gmail.com>
2023-08-26 18:14:47 +0200

    fix: gtk-themes

--- .local/share/dark-mode.d/gtk-theme.sh
+++ .local/share/dark-mode.d/gtk-theme.sh
@@ -3,11 +3,12 @@ gsettings set org.gnome.desktop.interface gtk-theme "$GTK_DARK_THEME" # anki and
 
 # old apps fix
 sed --in-place --follow-symlinks "s/'$GTK_LIGHT_THEME'/'$GTK_DARK_THEME'/" ~/.xsettingsd 
+sed --in-place --follow-symlinks "s/'$GTK_LIGHT_THEME'/'$GTK_DARK_THEME'/" ~/.config/gtk-4.0/settings.ini
 
 # multiple gtk versions
-ln -fs /usr/share/themes/Catppuccin-Mocha-Standard-Blue-dark/gtk-4.0/* ~/.config/gtk-4.0/
-ln -fs /usr/share/themes/Catppuccin-Mocha-Standard-Blue-dark/gtk-3.0/* ~/.config/gtk-3.0/
-ln -fs /usr/share/themes/Catppuccin-Mocha-Standard-Blue-dark/gtk-2.0/* ~/.config/gtk-2.0/
+ln -fs "/usr/share/themes/$GTK_DARK_THEME/gtk-4.0/*" ~/.config/gtk-4.0/
+ln -fs "/usr/share/themes/$GTK_DARK_THEME/gtk-3.0/*" ~/.config/gtk-3.0/
+ln -fs "/usr/share/themes/$GTK_DARK_THEME/gtk-2.0/*" ~/.config/gtk-2.0/
 
 if [[ "$STARTUP_DESKTOP" = "X11" ]]; then
   killall -hup xsettingsd
