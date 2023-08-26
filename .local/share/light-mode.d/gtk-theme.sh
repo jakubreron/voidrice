696196c0321fff99071cd36ec2ae4936812f9ff8
Jakub Reroń <kubareron@gmail.com>
2023-08-26 23:28:23 +0200

    fix: darkman switcher for gtk

--- .local/share/light-mode.d/gtk-theme.sh
+++ .local/share/light-mode.d/gtk-theme.sh
@@ -6,9 +6,9 @@ sed --in-place --follow-symlinks "s/'$GTK_DARK_THEME'/'$GTK_LIGHT_THEME'/" ~/.xs
 sed --in-place --follow-symlinks "s/'$GTK_DARK_THEME'/'$GTK_LIGHT_THEME'/" ~/.config/gtk-4.0/settings.ini
 
 # multiple gtk versions
-ln -fs "/usr/share/themes/$GTK_LIGHT_THEME/gtk-4.0/*" ~/.config/gtk-4.0/
-ln -fs "/usr/share/themes/$GTK_LIGHT_THEME/gtk-3.0/*" ~/.config/gtk-3.0/
-ln -fs "/usr/share/themes/$GTK_LIGHT_THEME/gtk-2.0/*" ~/.config/gtk-2.0/
+ln -fs /usr/share/themes/$GTK_LIGHT_THEME/gtk-4.0/* ~/.config/gtk-4.0/
+ln -fs /usr/share/themes/$GTK_LIGHT_THEME/gtk-3.0/* ~/.config/gtk-3.0/
+ln -fs /usr/share/themes/$GTK_LIGHT_THEME/gtk-2.0/* ~/.config/gtk-2.0/
 
 if [[ "$STARTUP_DESKTOP" = "X11" ]]; then
   killall -hup xsettingsd
