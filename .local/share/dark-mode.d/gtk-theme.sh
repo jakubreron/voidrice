4afdeb709515df032940726f6e97cf409d3047f3
Jakub Reroń <kubareron@gmail.com>
2023-08-27 00:36:38 +0200

    refactor: simplify variables in darkman

--- .local/share/dark-mode.d/gtk-theme.sh
+++ .local/share/dark-mode.d/gtk-theme.sh
@@ -1,18 +1,18 @@
-light_theme="Catppuccin-Latte-Standard-Blue-light"
-dark_theme="Catppuccin-Mocha-Standard-Blue-dark"
+theme="Catppuccin-Mocha-Standard-Blue-dark"
+# theme="adw-gtk3-dark"
 
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-gsettings set org.gnome.desktop.interface gtk-theme "$dark_theme" # anki and other apps that depend on string "-dark" fix
+gsettings set org.gnome.desktop.interface gtk-theme $theme # anki and other apps that depend on string "-dark" fix
 
 # old apps fix
-sed --in-place --follow-symlinks "s/$light_theme/$dark_theme/" ~/.config/gtk-4.0/settings.ini
+sed --in-place --follow-symlinks "s/gtk-theme-name=.*/gtk-theme-name=$theme/" ~/.config/gtk-3.0/settings.ini
+sed --in-place --follow-symlinks "s/gtk-theme-name=.*/gtk-theme-name=$theme/" ~/.config/gtk-4.0/settings.ini
 
 # multiple gtk versions
-ln -fs /usr/share/themes/$dark_theme/gtk-4.0/* ~/.config/gtk-4.0/
-ln -fs /usr/share/themes/$dark_theme/gtk-3.0/* ~/.config/gtk-3.0/
-ln -fs /usr/share/themes/$dark_theme/gtk-2.0/* ~/.config/gtk-2.0/
+ln -fs /usr/share/themes/$theme/gtk-4.0/* ~/.config/gtk-4.0/
+ln -fs /usr/share/themes/$theme/gtk-3.0/* ~/.config/gtk-3.0/
 
+sed --in-place --follow-symlinks "s/Net\/ThemeName.*/Net\/ThemeName \"$theme\"/" ~/.xsettingsd 
 if [[ "$STARTUP_DESKTOP" = "X11" ]]; then
-sed --in-place --follow-symlinks "s/$light_theme/$dark_theme/" ~/.xsettingsd 
   killall -hup xsettingsd
 fi
