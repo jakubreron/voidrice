3b50dfa8e98b4e25fb93d7c9ccfd29ebd2487df6
Jakub Reroń <kubareron@gmail.com>
2023-08-26 23:59:23 +0200

    fix: restore xsettingsd for x11

--- .local/share/dark-mode.d/gtk-theme.sh
+++ .local/share/dark-mode.d/gtk-theme.sh
@@ -5,10 +5,14 @@ gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 gsettings set org.gnome.desktop.interface gtk-theme "$dark_theme" # anki and other apps that depend on string "-dark" fix
 
 # old apps fix
-sed --in-place --follow-symlinks "s/$light_theme/$dark_theme/" ~/.xsettingsd 
 sed --in-place --follow-symlinks "s/$light_theme/$dark_theme/" ~/.config/gtk-4.0/settings.ini
 
 # multiple gtk versions
 ln -fs /usr/share/themes/$dark_theme/gtk-4.0/* ~/.config/gtk-4.0/
 ln -fs /usr/share/themes/$dark_theme/gtk-3.0/* ~/.config/gtk-3.0/
 ln -fs /usr/share/themes/$dark_theme/gtk-2.0/* ~/.config/gtk-2.0/
+
+if [[ "$STARTUP_DESKTOP" = "X11" ]]; then
+sed --in-place --follow-symlinks "s/$light_theme/$dark_theme/" ~/.xsettingsd 
+  killall -hup xsettingsd
+fi
