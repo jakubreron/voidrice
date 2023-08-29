b86d42606e1364482c02cd69e58b713b849ffa99
Jakub Reroń <kubareron@gmail.com>
2023-08-29 17:38:30 +0200

    fix: darkman not picking up vars from profile

--- .local/share/dark-mode.d/other-app-themes.sh
+++ .local/share/dark-mode.d/other-app-themes.sh
@@ -1,25 +1,30 @@
 #!/bin/sh
 
 dark_theme="mocha"
-dark_theme_alternative="macchiato"
 light_theme="latte"
+
+paths="$HOME/.config/alacritty/alacritty.yml
+  $HOME/.config/rofi/config.rasi
+  $HOME/.config/lvim/lua/user/builtin/init.lua
+  $HOME/.config/btop/btop.conf
+  $HOME/.config/fcitx5/conf/classicui.conf
+  $HOME/.config/waybar/style.css
+  $HOME/.config/tmux/tmux.conf
+"
 
 # TODO: add neovim
 # TODO: add git update-index assume-unchanged for all paths
+# TODO: try to integrate dunst import into the loop
 
-printf "%s" "$STANDARD_THEME_SWITCHER_PATHS" | while read -r path; do
+for path in "${paths[@]}"; do
   sed --in-place --follow-symlinks "s/$light_theme/$dark_theme/" $path
 done
 
-printf "%s" "$ALTERNATIVE_THEME_SWITCHER_PATHS" | while read -r path; do
-  sed --in-place --follow-symlinks "s/$light_theme/$dark_theme_alternative/" $path
-done
-
-# Dunst is different
 ln -fs ~/.config/dunst/themes/catppuccin/src/$dark_theme.conf ~/.config/dunst/dunstrc.d/99-theme.conf
 
 # Reload all after paths changes
-killall dunst && nohup dunst &
+killall dunst && sleep 1 && nohup dunst &
+
 if pgrep -f "tmux" > /dev/null; then
   tmux source-file ~/.config/tmux/tmux.conf
 fi
