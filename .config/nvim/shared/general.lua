005cff67b68907dad61a5776ac886081916dd764
Jakub Reroń <kubareron@gmail.com>
2024-03-26 20:01:50 +0900

    config(nvim): remove unused defaults

--- .config/nvim/shared/general.lua
+++ .config/nvim/shared/general.lua
@@ -3,20 +3,14 @@ local options = {
 	thesaurus = "~/.config/nvim/mthesaur.txt",
 	backup = false, -- creates a backup file
 	clipboard = "unnamedplus", -- allows neovim to access the system clipboard
-	cmdheight = 1, -- more space in the neovim command line for displaying messages
 	completeopt = { "menuone", "noselect" },
 	conceallevel = 0, -- so that `` is visible in markdown files
-	fileencoding = "utf-8", -- the encoding written to a file
-	foldmethod = "manual", -- folding, set to "expr" for treesitter based folding
-	foldexpr = "", -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
-	guifont = "monospace:h17", -- the font used in graphical neovim applications
 	hidden = true, -- required to keep multiple buffers and open multiple buffers
 	hlsearch = true, -- highlight all matches on previous search pattern
 	ignorecase = true, -- ignore case in search patterns
 	mouse = "a", -- allow the mouse to be used in neovim
 	pumheight = 10, -- pop up menu height
 	showmode = false, -- we don't need to see things like -- INSERT -- anymore
-	showtabline = 1, -- show tabline only if there are at least two tab pages (it's the default)
 	smartcase = true, -- smart case
 	smartindent = true, -- make indenting smarter again
 	splitbelow = true, -- force all horizontal splits to go below current window
@@ -28,23 +22,15 @@ local options = {
 	-- opt.titlestring = "%<%F%=%l/%L - nvim" -- what the title of the window will be set to
 	undofile = true, -- enable persistent undo
 	writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-	expandtab = true, -- convert tabs to spaces
-	shiftwidth = 2, -- the number of spaces inserted for each indentation
-	tabstop = 2, -- insert 2 spaces for a tab
 	cursorline = true, -- highlight the current line
 	relativenumber = true,
 	number = true, -- set numbered lines
-	numberwidth = 4, -- set number column width to 2 {default 4}
 	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
 	wrap = false, -- display lines as one long line
-	scrolloff = 8, -- minimal number of screen lines to keep above and below the cursor.
-	sidescrolloff = 8, -- minimal number of screen lines to keep left and right of the cursor.
-	showcmd = false,
-	ruler = false,
+	scrolloff = 10, -- minimal number of screen lines to keep above and below the cursor.
+	sidescrolloff = 10, -- minimal number of screen lines to keep left and right of the cursor.
 	laststatus = 3,
-	incsearch = true, -- Makes search act like search in modern browsers
 	lazyredraw = true, -- Don't redraw while executing macros (good performance config)
-	fileformats = "unix,dos,mac", -- Use Unix as the standard file type
 	linebreak = true, -- set linebreak on very long lines
 	textwidth = 500, -- linebreak on 500 characters
   colorcolumn = "80",
@@ -70,10 +56,6 @@ vim.opt.spelllang:append("cjk") -- disable spellchecking for asian characters (V
 vim.opt.shortmess:append("c") -- don't show redundant messages from ins-completion-menu
 vim.opt.shortmess:append("I") -- don't show the default intro message
 vim.opt.whichwrap:append("<,>,[,],h,l")
-
--- Undercurl
-vim.cmd([[let &t_Cs = "\e[4:3m"]])
-vim.cmd([[let &t_Ce = "\e[4:0m"]])
 
 for k, v in pairs(options) do
 	vim.opt[k] = v
