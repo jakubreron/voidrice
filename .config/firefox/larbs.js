877993e4b165e4e099d80817909014d389d0f5ba
Jakub Reroń <kubareron@gmail.com>
2024-08-08 01:03:21 +0900

    cleanup: firefox unused overrides

--- .config/firefox/larbs.js
+++ .config/firefox/larbs.js
@@ -3,102 +3,19 @@
  * [SETTING] Privacy & Security>Logins and Passwords>Ask to save logins and passwords for websites ***/
 user_pref("signon.rememberSignons", false);
 
-/* UPDATES ***/
-user_pref("app.update.auto", false); // [NON-WINDOWS] disable auto app updates
-
 /* APPEARANCE ***/
 user_pref("browser.download.autohideButton", true); // [FF57+]
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // [FF68+] allow userChrome/userContent
 
 /* UX BEHAVIOR ***/
 user_pref("general.autoScroll", false); // middle-click enabling auto-scrolling [DEFAULT: false on Linux]
 user_pref("ui.key.menuAccessKey", 0); // disable alt key toggling the menu bar [RESTART]
 
 /* UX FEATURES ***/
-user_pref("extensions.pocket.enabled", true); // Pocket Account [FF46+]
 user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
-
 user_pref("xpinstall.signatures.required", false);
-
-// Disable the Twitter/R*ddit/Faceberg ads in the URL bar:
-user_pref("browser.urlbar.quicksuggest.enabled", false);
-user_pref("browser.urlbar.suggest.topsites", false); // [FF78+]
 
 /* WARNINGS ***/
 user_pref("full-screen-api.warning.timeout", 0); // disable "... is now fullscreen" warning
 
-// WARNING: dangerous options
-/* 0102: set startup page [SETUP-CHROME]
- * 0=blank, 1=home, 2=last visited page, 3=resume previous session
- * [NOTE] Session Restore is cleared with history (2811, 2820), and not used in Private Browsing mode
- * [SETTING] General>Startup>Restore previous session ***/
+// 0=blank, 1=home, 2=last visited page, 3=resume previous session
 user_pref("browser.startup.page", 3); // default 0
-
-// NOTE: next-navigation performance
-/* 0601: disable link prefetching
- * [1] https://developer.mozilla.org/docs/Web/HTTP/Link_prefetching_FAQ ***/
-user_pref("network.prefetch-next", true);
-
-/* 0602: disable DNS prefetching
- * [1] https://developer.mozilla.org/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control ***/
-user_pref("network.dns.disablePrefetch", false);
-
-/* 0603: disable predictor / prefetching ***/
-user_pref("network.predictor.enabled", true);
-user_pref("network.predictor.enable-prefetch", true); // [FF48+] [DEFAULT: false]
-
-/* 0801: disable location bar using search
- * Don't leak URL typos to a search engine, give an error message instead
- * Examples: "secretplace,com", "secretplace/com", "secretplace com", "secret place.com"
- * [NOTE] This does not affect explicit user action such as using search buttons in the
- * dropdown, or using keyword search shortcuts you configure in options (e.g. "d" for DuckDuckGo)
- * [SETUP-CHROME] Override this if you trust and use a privacy respecting search engine ***/
-user_pref("keyword.enabled", true);
-
-/* 1601: control when to send a cross-origin referer
- * 0=always (default), 1=only if base domains match, 2=only if hosts match
- * [SETUP-WEB] Breakage: older modems/routers and some sites e.g banks, vimeo, icloud, instagram
- * If "2" is too strict, then override to "0" and use Smart Referer extension (Strict mode + add exceptions) ***/
-user_pref("network.http.referer.XOriginPolicy", 0);
-
-/** COOKIES + SITE DATA : ALLOWS EXCEPTIONS ***/
-/* 2801: delete cookies and site data on exit
- * 0=keep until they expire (default), 2=keep until you close Firefox
- * [NOTE] A "cookie" block permission also controls localStorage/sessionStorage, indexedDB,
- * sharedWorkers and serviceWorkers. serviceWorkers require an "Allow" permission
- * [SETTING] Privacy & Security>Cookies and Site Data>Delete cookies and site data when Firefox is closed
- * [SETTING] to add site exceptions: Ctrl+I>Permissions>Cookies>Allow
- * [SETTING] to manage site exceptions: Options>Privacy & Security>Permissions>Settings ***/
-user_pref("network.cookie.lifetimePolicy", 0);
-
-/* 2810: enable Firefox to clear items on shutdown (2811)
- * [NOTE] Exceptions: A "cookie" block permission also controls "offlineApps" (see note in 2811).
- * serviceWorkers require an "Allow" permission. For cross-domain logins, add exceptions for
- * both sites e.g. https://www.youtube.com (site) + https://accounts.google.com (single sign on)
- * [WARNING] Be selective with what cookies you keep, as they also disable partitioning (1767271)
- * [SETTING] Privacy & Security>History>Custom Settings>Clear history when Firefox closes
- * [SETTING] to add site exceptions: Ctrl+I>Permissions>Cookies>Allow (when on the website in question)
- * [SETTING] to manage site exceptions: Options>Privacy & Security>Permissions>Settings ***/
-user_pref("privacy.sanitize.sanitizeOnShutdown", false);
-user_pref("privacy.clearOnShutdown.cookies", false); // Cookies
-
-/* 4520: disable WebGL (Web Graphics Library)
- * [SETUP-WEB] If you need it then override it. RFP still randomizes canvas for naive scripts ***/
-user_pref("webgl.disabled", false);
-
-/* 4501: enable privacy.resistFingerprinting [FF41+]
- * [SETUP-WEB] RFP can cause some website breakage: mainly canvas, use a site exception via the urlbar
- * RFP also has a few side effects: mainly timezone is UTC0, and websites will prefer light theme
- * [1] https://bugzilla.mozilla.org/418986 ***/
-user_pref("privacy.resistFingerprinting", false);
-
-/* 4504: enable RFP letterboxing [FF67+]
- * Dynamically resizes the inner window by applying margins in stepped ranges [2]
- * If you use the dimension pref, then it will only apply those resolutions.
- * The format is "width1xheight1, width2xheight2, ..." (e.g. "800x600, 1000x1000")
- * [SETUP-WEB] This is independent of RFP (4501). If you're not using RFP, or you are but
- * dislike the margins, then flip this pref, keeping in mind that it is effectively fingerprintable
- * [WARNING] DO NOT USE: the dimension pref is only meant for testing
- * [1] https://bugzilla.mozilla.org/1407366
- * [2] https://hg.mozilla.org/mozilla-central/rev/6d2d7856e468#l2.32 ***/
-user_pref("privacy.resistFingerprinting.letterboxing", false); // [HIDDEN PREF]
