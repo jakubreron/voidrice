4fefa032a7fa59e340db674d9440bad9418faa7d
Jakub Reroń <kubareron@gmail.com>
2024-10-05 13:27:17 +0900

    feat(themes): switch kvantum theme

--- .local/share/dark-mode.d/kde-gtk-theme.sh
+++ .local/share/dark-mode.d/kde-gtk-theme.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-theme="adw-gtk3-dark"
+gtk_theme="adw-gtk3-dark"
+kvantum_theme="KvLibadwaitaDark"
 
-"$XDG_BIN_HOME"/themes/switch-kde-theme-to "$theme"
+"$XDG_BIN_HOME"/themes/switch-kde-theme-to "$gtk_theme" "$kvantum_theme"
--- .local/share/light-mode.d/kde-gtk-theme.sh
+++ .local/share/light-mode.d/kde-gtk-theme.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-theme="adw-gtk3"
+gtk_theme="adw-gtk3"
+kvantum_theme="KvLibadwaita"
 
-"$XDG_BIN_HOME"/themes/switch-kde-theme-to "$theme"
+"$XDG_BIN_HOME"/themes/switch-kde-theme-to "$gtk_theme" "$kvantum_theme"
