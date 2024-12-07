d604e695c5b45e93e088c76968274848a4960dc7
Jakub Reroń <kubareron@gmail.com>
2024-12-07 12:31:01 +0900

    config(darkman): quote prefer-dark prefer-light

--- .local/share/dark-mode.d/gtk-theme.sh
+++ .local/share/dark-mode.d/gtk-theme.sh
@@ -5,4 +5,4 @@ theme="adw-gtk3-dark"
 
 "$XDG_BIN_HOME"/themes/set-gtk-theme $theme
 
-gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
