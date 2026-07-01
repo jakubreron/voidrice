f4c7ea69ca6d46ae4cf2df037fb62baf53c0c78c
Jakub Reroń <kubareron@gmail.com>
2026-07-01 23:27:50 +0900

    feat(hypr): no border on one window

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -194,6 +194,13 @@ end
 ----------------------
 ---- WINDOW RULES ----
 ----------------------
+-- Smart Gaps: Remove gaps if there is only 1 visible tiled window, or when maximized
+hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
+hl.workspace_rule({ workspace = "f[1]", gaps_out = 0, gaps_in = 0 })
+
+-- Smart Borders: Remove borders and rounding under the same conditions
+hl.window_rule({ match = { float = false, workspace = "w[tv1]" }, border_size = 0, rounding = 0 })
+hl.window_rule({ match = { float = false, workspace = "f[1]" }, border_size = 0, rounding = 0 })
 
 -- Browser
 hl.window_rule({ name = "browser-ws", match = { class = "^(.*)(firefox)(.*)$" }, workspace = "1" })
